3b4f1b2ddefbab77af696ec0a3d4121d88720067
Iramsey0418 <saiah.ramsey18@gmail.com>
2018-12-12 11:05:31 -0600

    inti

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cali-Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pepper Jack  Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', true);
